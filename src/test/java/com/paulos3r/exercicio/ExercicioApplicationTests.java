9da1c46ee497a7c1a5fb097e5153eecf77366866
Paulo Sergio <036paulo@gmail.com>
2025-04-23 21:12:22 -0300

    Projeto inicial com spring, lombok, mysql, flyway

---
+++ src/test/java/com/paulos3r/exercicio/ExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulos3r.exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
